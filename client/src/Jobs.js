98034e0501a23e22e34ac2faaff289687bc41c19
Guillaume Fontorbe <gfontorbe@gmail.com>
2021-08-31 23:32:46 +0200

    Initial Commit

---
+++ client/src/Jobs.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import Typography from '@material-ui/core/Typography';
+
+import Job from './Job';
+
+function Jobs() {
+    return(
+        <div>
+            <Typography variant="h1">
+                Entry Level Software Jobs
+            </Typography>
+        </div>
+    )
+}
+
+export default Jobs;
