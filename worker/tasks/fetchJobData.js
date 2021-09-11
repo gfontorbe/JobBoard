219db7d8de7601b186b641334d77daf2a8d02db4
Guillaume Fontorbe <gfontorbe@gmail.com>
2021-09-11 22:10:16 +0200

    Exclude jobs already in memory

--- worker/tasks/fetchJobData.js
+++ worker/tasks/fetchJobData.js
@@ -1,5 +1,6 @@
 import scrapeIndeed from "./scraperIndeed.js";
 import { createClient } from "redis";
+import { json } from "express";
 
 const baseURL = "https://de.indeed.com/Jobs?q=web+developer";
 
@@ -9,15 +10,21 @@ export async function fetchJobsData() {
   client.on("error", (err) => console.log("Redis Client Error", err));
 
   const allJobs = await scrapeIndeed(`${baseURL}&sort=date`);
-  
+
   // filter jobs
   const filteredJobs = [];
+  await client.connect();
+  const jobsInMemory = await client.get("indeedJobs");  
 
-  console.log(`filtering jobs`);
+  console.log(`filtering jobs...`);
   for (let index = 0; index < allJobs.length; index++) {
     const element = allJobs[index];
-
-    //remove ads containing "senior"
+    // remove jobs already in memory
+    if (jobsInMemory.includes(element.id)) {
+      console.log(`job ${element.id} already in memory`);
+      continue;
+    }
+    // remove ads containing "senior"
     if (element.title.toLowerCase().includes("senior")) {
       console.log(`filtered out ad id:${element.id} title:${element.title}`);
       continue;
@@ -30,13 +37,18 @@ export async function fetchJobsData() {
     `filtered jobs: ${filteredJobs.length} left from ${allJobs.length}`
   );
 
-  //set in Redis
-  await client.connect();
-  await client.set("indeedJobs", JSON.stringify(filteredJobs));
+  // add new filtered jobs to memory
+  const newJobList = (JSON.stringify(filteredJobs) + jobsInMemory).replace(
+    "][",
+    ","
+  );
 
-  const testValue = await client.get("indeedJobs");
+  //set in Redis
+  await client.set("indeedJobs", newJobList);
 
   const endTime = Date.now();
 
   console.log(`Process done in ${endTime - startTime}ms.`);
 }
+
+fetchJobsData();
