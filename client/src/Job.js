9c14964ce3ac617ef2943f2540491f60f4530a91
Guillaume Fontorbe <gfontorbe@gmail.com>
2021-09-12 13:26:07 +0200

    update job component style

--- client/src/Job.js
+++ client/src/Job.js
@@ -7,7 +7,7 @@ export default function Job({ job, onClick }) {
       <div>
         <Typography variant="h5">{job.title}</Typography>
         <Typography variant="h6">{job.company}</Typography>
-        <Typography>{job.location}</Typography>
+        <Typography variant="h6">{job.location}</Typography>
       </div>
       <div>
         <Typography>{Date(job.fetchedOn).split(' ').slice(1,4).join(' ')}</Typography>
